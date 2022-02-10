944f41fb9431e45b2b2b3730c30b139026a5dcb5
Clay Smith <clay.smith@servicenow.com>
2022-02-10 11:08:40 -0800

    bump version to v1.60.0

--- modules/ec2-alerts/main.tf
+++ modules/ec2-alerts/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = ">= 1.51.2"
+      version = ">= 1.60.0"
     }
   }
   required_version = ">= v1.0.11"
@@ -10,32 +10,26 @@ terraform {
 
 resource "lightstep_metric_condition" "ec2_cpu_usage" {
   project_name = var.lightstep_project
-  name         = "EC2 CPU Usage"
+  name         = "EC2 CPU Utilization"
 
   expression {
     evaluation_window   = "2m"
     evaluation_criteria = "on_average"
     is_no_data          = true
-    operand             = "below"
+    operand             = "above"
     thresholds {
-      warning  = 9
-      critical = 8
+      warning  = 80
+      critical = 90
     }
   }
 
   metric_query {
-    metric              = "requests"
+    metric              = "aws.ec2.cpu_utilization_max"
     query_name          = "a"
     timeseries_operator = "delta"
     hidden              = false
     display             = "line"
 
-    include_filters = [
-      {
-        key   = "kube_instance"
-        value = "3"
-      }
-    ]
 
     group_by {
       aggregation_method = "max"
